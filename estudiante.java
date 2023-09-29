6a7331dc5f74a25f58e8059f12c0f411e11d4a1c
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-09-29 15:26:20 -0500

    Create estudiante.java

---
+++ estudiante.java
@@ -0,0 +1,64 @@
+/**
+ * Clase Teclado
+ */
+public class estudiante {
+    /***************************************************/
+    /******************** Atributos ********************/
+    /***************************************************/
+   String nombre;
+Date fecha de nacimiento;
+double notaMateria1;
+double notaMateria2;
+double notaMateria3;
+
+    /***************************************************/
+    /******************** Métodos ********************/
+    /***************************************************/
+
+    /**
+     * Constructor de la clase estudiante: Inicializa los atributos de mi clase
+     * @param String
+     * @param Date
+     * @param double((notaMateria1+notaMateria2+notaMateria3)/3);
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    public Estudiante(String nombre,Date fecha de nacimiento, double notaMateria1, double notaMateria2, double notaMateria3){
+        this.marca = marca;
+        this.teclas = teclas;
+        this.tipo = tipo;
+    }
+
+    /**
+     * Método de imprimir el nombre del estudiante
+     * @return boolean: Imprime el nombre del estudiante
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    boolean NombreEstudiante(){
+        System.out.println("El nombre del estudiantes es " +  NombreEstudiante);
+        return true;
+    }
+
+    /**
+     * Método que imprime la edad del estudiante
+     * @return boolean:Imprime la edad que tiene el estudiante según su año de nacimientos
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    boolean edadEstudiante(){
+        System.out.println("La edad del estudiate  "+ nombreEstudiante " es " + edadEstudiante );
+        return true;
+    }
+
+    /**
+     * Método que hace el promedio de las 3 notas
+     * @return boolean: Indica su promedio final dependiendo de las 3 notas dadas
+     *
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    boolean promedioEstudiante(){
+        System.out.println("El promedio de " + nombreEstudiante + " es " + promedioEstdiante);
+        return true;
+    }
+}
