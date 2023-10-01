511ab98dbdea7b25c8c9d586625d8a7ee00a0fae
wadoooh <142348480+wadoooh@users.noreply.github.com>
2023-09-30 21:22:35 -0500

    Update estudiante.java

--- estudiante.java
+++ estudiante.java
@@ -1,12 +1,12 @@
 /**
- * Clase Teclado
+ * Clase estudiante
  */
 public class estudiante {
     /***************************************************/
     /******************** Atributos ********************/
     /***************************************************/
-   String nombre;
-Date fecha de nacimiento;
+ private  String nombre;
+private Date fechaDeNacimiento;
 private double notaMateria1;
 private double notaMateria2;
 private double notaMateria3;
@@ -23,23 +23,22 @@ private double notaMateria3;
      *
      * Complejidad temporal: O(1) Tiempo Constante
      */
-    public Estudiante(String nombre,Date fecha de nacimiento, double notaMateria1, double notaMateria2, double notaMateria3){
-        this.marca = marca;
-        this.teclas = teclas;
-        this.tipo = tipo;
+   public Estudiante(String nombre, Date fechaDeNacimiento) {
+        this.nombre = nombre;
+        this.fechaDeNacimiento = fechaDeNacimiento; 
     }
-
+public String getNombre() { 
+        return nombre;
+    }
     /**
      * Método de imprimir el nombre del estudiante
-     * @return boolean: Imprime el nombre del estudiante
+     * @return String: Imprime el nombre del estudiante
      *
      * Complejidad temporal: O(1) Tiempo Constante
      */
-    boolean NombreEstudiante(){
-        System.out.println("El nombre del estudiantes es " +  NombreEstudiante);
-        return true;
+    public void setNombre(String nombre) {
+        this.nombre = nombre; 
     }
-
     /**
      * Método que imprime la edad del estudiante
      *Primero solicita el año de nacimiento del estudiante y lo suma mas el año actual
@@ -47,32 +46,71 @@ private double notaMateria3;
      *
      * Complejidad temporal: O(1) Tiempo Constante
      */
-    boolean edadEstudiante(){
-        System.out.println("La edad del estudiate  "+ nombreEstudiante " es " + edadEstudiante );
-        return true;
-        import java.util.Scanner;
-
-private class CalcularEdad {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingresa tu año de nacimiento: ");
-        int añoNacimiento = scanner.nextInt();
-        int añoActual = 2023;
-        int edadEstudiante = añoNacimiento - añoActual;
-        }
-  scanner.close();
-    }
-}
+    public Date getFechaDeNacimiento() {
+        return fechaDeNacimiento;
     }
 
-    /**
-     * Método que hace el promedio de las 3 notas
-     * @return boolean: Indica su promedio final dependiendo de las 3 notas dadas
+    public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+ /**
+     * Método para las 3 notas del estudiante
+     *Primero solicita las 3 notas del estudiante para despues sacarle el promedio
+     * @return double:toma las 3 notas del estudiante
      *
      * Complejidad temporal: O(1) Tiempo Constante
      */
-    boolean promedioEstudiante(){
-        System.out.println("El promedio de " + nombreEstudiante + " es " + promedioEstdiante);
-        return true;
+    public double getnotaMateria1() {
+        return notaMateria1;
+    }
+
+    public void setnotaMateria1(double notaMateria1) {
+        this.notaMateria1 = notaMateria1;
+    }
+
+    public double getnotaMateria2() {
+        return notaMateria2;
+    }
+
+    public void setnotaMateria2(double notaMateria2) {
+        this.notaMateria2 = notaMateria2;
+    }
+
+    public double getnotaMateria3() {
+        return notaMateria3;
+    }
+
+    public void setnotaMateria3(double notaMateria3) {
+        this.notaMateria3 = notaMateria3;
+    }
+
+    /******************** Método para la edad del estudiante ********************/
+    /**
+     * Método que calcula la edad actual del estudiante en años
+     * @return La edad actual del estudiante en años
+     */
+    /**
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+    public int getEdad() {
+        Date fechaActual = new Date();
+        int edad = fechaActual.getAnio() - fechaDeNacimiento.getAnio();
+        if (fechaDeNacimiento.getMes() > fechaActual.getMes() || 
+           (fechaDeNacimiento.getMes() == fechaActual.getMes() && 
+            fechaDeNacimiento.getDate() > fechaActual.getDate())) {
+            edad--;
+        }
+        return edad;
+    }
+      /******************** Método para el promedio del estudiante ********************/
+    /**
+     * Método que calcula el promedio del estudiante dependiendo de las 3 notas
+     * @return El promedio
+     */
+    /**
+     * Complejidad temporal: O(1) Tiempo Constante
+     */
+     public double promedioEstudiante(){
+    return (notaMateria1 + notaMateria2 + notaMateria3) / 3;
     }
 }
